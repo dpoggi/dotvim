80c32a7e0667c1b93ab6214b6c8a92a049921c74
Dan Poggi <dan.poggi@gmail.com>
2011-07-12 18:05:15 -0400

    Added script for creating links to vimrc and gvimrc

---
+++ create_links.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+['vimrc', 'gvimrc'].each do |file|
+  original = File.expand_path file
+  link = File.join File.expand_path('~'), ".#{File.basename file}"
+  if not File.exists? link
+    FileUtils.ln_s original, link
+  end
+end
