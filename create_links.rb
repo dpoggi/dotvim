7167da31790bfb8dd0cae9718faa90cb988c04ca
Dan Poggi <dan.poggi@gmail.com>
2011-07-12 18:15:36 -0400

    Updated link script

--- create_links.rb
+++ create_links.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 ['vimrc', 'gvimrc'].each do |file|
   original = File.expand_path file
   link = File.join File.expand_path('~'), ".#{File.basename file}"
+  puts "#{link} => #{original}"
   if not File.exists? link
     FileUtils.ln_s original, link
   end
