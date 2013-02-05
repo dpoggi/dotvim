cec53d1560cd80e32fa3fc71f1f88ed6ee8a7e03
Dan Poggi <dan.poggi@gmail.com>
2013-02-05 02:35:58 -0500

    Fixed update_colors script

--- update_colors.sh
+++ update_colors.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # Fetch ze colorschemes, for they are nowhere on Github :(
 
-hash curl 2>&- && DCP_GET="curl -kLJO"
-hash wget 2>&- && DCP_GET="wget --no-check-certificate --content-disposition"
-test "$DCP_GET" || { echo >&2 "Error! Couldn't find cURL or Wget!"; exit 1; }
+hash curl 2>&- && GET="curl -kLJO"
+hash wget 2>&- && GET="wget --no-check-certificate --content-disposition"
+test "$GET" || { echo >&2 "Error! Couldn't find cURL or Wget!"; exit 1; }
 
-OLD_WD=`pwd`
 cd "$HOME/.vim/colors"
 rm -f *.vim
 
@@ -14,8 +13,6 @@ for URL in \
   "http://blog.toddwerth.com/entry_files/8/ir_black.vim" \
   "http://www.vim.org/scripts/download_script.php?src_id=11225"
 do
-  $DCP_GET "$URL"
+  $GET "$URL"
   echo
 done
-
-cd "$OLD_WD"
