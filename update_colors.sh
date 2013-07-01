21a4b83c54baf2ae9c555371516f92d3b5ea353b
Dan Poggi <dan.poggi@gmail.com>
2013-07-01 08:13:16 -0400

    Added solarized colorscheme

--- update_colors.sh
+++ update_colors.sh
@@ -11,7 +11,8 @@ rm -f *.vim
 for URL in \
   "http://www.vim.org/scripts/download_script.php?src_id=9750" \
   "http://blog.toddwerth.com/entry_files/8/ir_black.vim" \
-  "http://www.vim.org/scripts/download_script.php?src_id=11225"
+  "http://www.vim.org/scripts/download_script.php?src_id=11225" \
+  "https://raw.github.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim"
 do
   $GET "$URL"
   echo
