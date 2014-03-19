b638b836dc029092a539687aa83d2acc58b1c6ea
Dan Poggi <dan.poggi@gmail.com>
2014-03-18 23:43:08 -0400

    base16-default -> base16-railscasts

--- update_colors.sh
+++ update_colors.sh
@@ -17,7 +17,7 @@ for url in \
   "http://www.vim.org/scripts/download_script.php?src_id=11225" \
   "https://raw.github.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim" \
   "https://raw.github.com/vim-scripts/Zenburn/master/colors/zenburn.vim" \
-  "https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-default.vim"
+  "https://raw.githubusercontent.com/chriskempson/base16-vim/master/colors/base16-railscasts.vim"
 do
   ${curl_cmd} "${url}"
   echo
