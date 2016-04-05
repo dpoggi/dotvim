31605513fa695265e22ac2a5e286815e06f2f149
Dan Poggi <dan.poggi@detroitlabs.com>
2016-04-05 13:12:49 -0400

    Better submodule logic

--- update_submodules.sh
+++ update_submodules.sh
@@ -4,15 +4,17 @@
 # Updates Vim plugin submodules from their respective origins
 #
 
+uses_develop=( "powerline" )
+
 cd "${HOME}/.vim"
-printf >&2 "Fetching origin recursively (whole repo)...\n"
+printf >&2 "Fetching origin recursively...\n"
 git fetch origin --recurse-submodules
 printf >&2 "\n"
 
 cd bundle
 for submodule in *; do
   pushd "${submodule}" >/dev/null
-  [[ "${submodule}" = "powerline" ]] && ref="origin/develop" || ref="origin/master"
+  [[ " ${uses_develop[@]} " =~ " ${submodule} " ]] && ref="origin/develop" || ref="origin/master"
 
   printf >&2 "Attempting to merge ${ref} for ${submodule}...\n"
   git merge --ff-only --no-edit "${ref}"
