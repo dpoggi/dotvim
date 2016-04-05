d2c3eac7d398d148c81ce282c94977a145ae3407
Dan Poggi <dan.poggi@detroitlabs.com>
2016-04-05 13:00:00 -0400

    Better submodule updater

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-
+#
 # update_submodules.sh
 # Updates Vim plugin submodules from their respective origins
+#
 
 cd "${HOME}/.vim"
+printf >&2 "Fetching origin recursively (whole repo)...\n"
 git fetch origin --recurse-submodules
+printf >&2 "\n"
 
 cd bundle
 for submodule in *; do
   pushd "${submodule}" >/dev/null
+  [[ "${submodule}" = "powerline" ]] && ref="origin/develop" || ref="origin/master"
 
-  if [[ "${submodule}" = "powerline" ]]; then
-    git rebase refs/remotes/origin/develop
-  else
-    git rebase refs/remotes/origin/master
-  fi
+  printf >&2 "Attempting to merge ${ref} for ${submodule}...\n"
+  git merge --ff-only --no-edit "${ref}"
+  printf >&2 "Updating submodules for ${submodule}, if any...\n"
+  git submodule update --init --recursive
+  printf >&2 "\n"
 
   popd >/dev/null
 done
