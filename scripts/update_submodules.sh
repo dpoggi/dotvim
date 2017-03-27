b8b110b23bf3e0729397ffb34a9a9f21183d4e07
Dan Poggi <dan.poggi@gmail.com>
2017-03-27 09:48:43 -0400

    Improvements to update_submodules.sh

--- scripts/update_submodules.sh
+++ scripts/update_submodules.sh
@@ -4,40 +4,68 @@
 # Updates Vim plugin submodules from their respective origins
 #
 
-cd "$(dirname "${BASH_SOURCE[0]}")/.."
+set -e
 
-printf >&2 "Fetching origin recursively...\n"
-git fetch origin --recurse-submodules
-printf >&2 "\n"
+log_info() {
+  printf "\033[0;34m[%s]:\033[0m %s\n" \
+         "$(date "+%Y-%m-%d %H:%M:%S")" \
+         "$*"
+}
 
-# Pathogen is deliberately excluded as its updates are sometimes more involved.
-for submodule in bundle/*; do
-  plugin="$(basename "${submodule}")"
-  pushd "${submodule}" > /dev/null
+log_error() {
+  printf >&2 "\033[1;37;41m[%s]:\033[0m %s\n" \
+             "$(date "+%Y-%m-%d %H:%M:%S")" \
+             "$*"
+}
 
-  # The JavaScript plugin's master branch is a little... behind.
-  if [[ "${plugin}" = "javascript" ]]; then
-    symbolic_ref="refs/remotes/origin/develop"
-  else
-    symbolic_ref="$(git symbolic-ref refs/remotes/origin/HEAD 2> /dev/null)"
-  fi
+main() {
+  cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-  # refs/remotes/origin/branch -> sha1
-  if [[ -n "${symbolic_ref}" ]]; then
-    ref="$(git show-ref --hash --verify "${symbolic_ref}" 2> /dev/null)"
-  fi
+  log_info "Fetching origin recursively..."
 
-  if [[ -z "${symbolic_ref}" || -z "${ref}" ]]; then
-    printf >&2 "Error: unable to establish remote branch for ${plugin}, skipping...\n"
-    popd > /dev/null
-    continue
-  fi
+  git fetch origin --recurse-submodules
 
-  printf >&2 "Attempting to checkout ${symbolic_ref} for ${plugin}...\n"
-  git checkout --force "${ref}"
-  printf >&2 "Updating submodules for ${plugin}, if any...\n"
-  git submodule update --init --recursive
   printf >&2 "\n"
 
-  popd > /dev/null
-done
+  local submodule plugin symbolic_ref ref
+
+  for submodule in bundle/*; do
+    plugin="$(basename "${submodule}")"
+
+    pushd "${submodule}" > /dev/null
+
+    # The JavaScript plugin's master branch is a little... behind
+    if [[ "${plugin}" = "javascript" ]]; then
+      symbolic_ref="refs/remotes/origin/develop"
+    else
+      symbolic_ref="$(git symbolic-ref refs/remotes/origin/HEAD 2> /dev/null)"
+    fi
+
+    # refs/remotes/origin/branch -> sha1
+    if [[ -n "${symbolic_ref}" ]]; then
+      ref="$(git show-ref --hash --verify "${symbolic_ref}" 2> /dev/null)"
+    fi
+
+    if [[ -z "${symbolic_ref}" || -z "${ref}" ]]; then
+      log_error "Error: unable to establish remote branch for ${plugin}, skipping..."
+
+      popd > /dev/null
+
+      continue
+    fi
+
+    log_info "Attempting to checkout ${symbolic_ref} for ${plugin}..."
+
+    git checkout --force "${ref}"
+
+    log_info "Updating submodules for ${plugin}, if any..."
+
+    git submodule update --init --recursive
+
+    printf >&2 "\n"
+
+    popd > /dev/null
+  done
+}
+
+main "$@"
