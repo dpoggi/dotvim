efc3c5bbfde9db85cf5491858ecc7be2a0f35388
Dan Poggi <dan.poggi@gmail.com>
2017-03-27 09:18:39 -0400

    Further improvements to post_update.sh

--- scripts/post_update.sh
+++ scripts/post_update.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+#
+# post_update.sh
+# Post-update hook to ensure Vimproc is built, handle submodule removal, and
+# regenerate helptags.
+#
 
 set -e
 
-readonly VIM_DIR="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
+readonly VIM_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 readonly EFFECTIVE_HOME="$(cd "${VIM_DIR}/.." && pwd -P)"
 readonly GIT_MODULES="${VIM_DIR}/.gitmodules"
 readonly GIT_CONFIG="${VIM_DIR}/.git/config"
@@ -13,97 +18,84 @@ log_info() {
          "$*"
 }
 
-log_error() {
-  printf >&2 "\033[1;37;41m[%s]:\033[0m %s\n" \
-             "$(date "+%Y-%m-%d %H:%M:%S")" \
-             "$*"
-}
-
-MAKE_VIMPROC="true"
-
-# Don't try to make vimproc if it's being ignored.
-if grep -Fq "vimproc" "${VIM_DIR}/plugins.local" &> /dev/null; then
-  MAKE_VIMPROC="false"
-fi
-
-# Don't try to make vimproc if we're on a Mac without Xcode/CLI tools
-# (/usr/bin/make will be present but trigger a GUI installer).
-if [[ "$(uname -s)" = "Darwin" ]]; then
-  if ! xcode-select --print-path &> /dev/null; then
-    MAKE_VIMPROC="false"
+build_vimproc() {
+  # Don't try to build vimproc if it's being ignored in ~/.vim/plugins.local
+  if grep -Fq 'vimproc' "${VIM_DIR}/plugins.local" 2> /dev/null; then
+    return
   fi
-fi
+
+  # Don't try to build vimproc if we're on macOS without Xcode or CLI tools
+  # installed (/usr/bin/make will be present but trigger a GUI installer)
+  if [[ "$(uname -s)" = "Darwin" ]] \
+     && ! xcode-select --print-path &> /dev/null; then
+    return
+  fi
+
+  log_info "(Re)building vimproc..."
+
+  HOME="${EFFECTIVE_HOME}" vim -c 'silent VimProcInstall' -c 'qall!'
+}
 
 remove_dir() {
-  if [[ ! -e "$1" ]]; then
+  local dir_path="$1"
+  local dir_description="$2"
+  local plugin_repo="$3"
+
+  if [[ ! -d "${dir_path}" ]]; then
     return
   fi
 
-  log_info "Removing $2 for $3..."
+  log_info "Removing ${dir_description} for ${plugin_repo}..."
 
-  rm -rf "$1"
+  rm -rf "${dir_path}"
 }
 
-clean_up() {
-  if ! grep -Fq "bundle/$3" "$1" &> /dev/null; then
+scrub_file() {
+  local file_path="$1"
+  local file_description="$2"
+  local plugin_bundle="$3"
+  local plugin_repo="$4"
+
+  if ! grep -Fq "bundle/${plugin_bundle}" "${file_path}" \
+     && ! grep -Fq "/${plugin_repo}.git" "${file_path}"; then
     return
   fi
 
-  log_info "Cleaning up $2 for $4..."
+  log_info "Scrubbing ${plugin_repo} from ${file_description}..."
 
   perl -i -ln \
-    -e "print unless (/bundle\/$3/ || /\/$4.git/)" \
-    "$1"
+       -e "print unless (/bundle\\/${plugin_bundle}/ || /\\/${plugin_repo}\\.git/)" \
+       "${file_path}"
 }
 
 remove_submodule() {
-  remove_dir "${VIM_DIR}/bundle/$1" \
+  local bundle="$1"
+  local repo="$2"
+
+  remove_dir "${VIM_DIR}/bundle/${bundle}" \
              "plugin bundle" \
-             "$2"
+             "${repo}"
 
-  remove_dir "${VIM_DIR}/.git/modules/bundle/$1" \
+  remove_dir "${VIM_DIR}/.git/modules/bundle/${bundle}" \
              "orphaned submodule" \
-             "$2"
+             "${repo}"
 
-  clean_up "${GIT_MODULES}" \
-           ".gitmodules" \
-           "$1" \
-           "$2"
+  scrub_file "${GIT_MODULES}" \
+             ".gitmodules" \
+             "${bundle}" \
+             "${repo}"
 
-  clean_up "${GIT_CONFIG}" \
-           ".git/config" \
-           "$1" \
-           "$2"
+  scrub_file "${GIT_CONFIG}" \
+             ".git/config" \
+             "${bundle}" \
+             "${repo}"
 }
 
-if [[ "${MAKE_VIMPROC}" = "true" ]]; then
-  log_info "Rebuilding vimproc..."
+migrate_thrift() {
+  if ! grep -Fq 'sprsquish' "${GIT_CONFIG}"; then
+    return
+  fi
 
-  HOME="${EFFECTIVE_HOME}" vim -c "silent VimProcInstall" -c "qall!"
-fi
-
-for plugin in \
-  "CamelCaseMotion:CamelCaseMotion" \
-  "clojure:VimClojure" \
-  "cocoa:cocoa.vim" \
-  "command-t:Command-T" \
-  "elm:elm-vim" \
-  "funcoo:funcoo.vim" \
-  "haml:vim-haml" \
-  "ios:vim-ios" \
-  "kiwi:kiwi.vim" \
-  "mako:mako.vim" \
-  "nginx:nginx.vim" \
-  "powerline:powerline" \
-  "systemverilog:systemverilog.vim" \
-  "ts:tsuquyomi"
-do
-  IFS=":" read -r bundle repo <<< "${plugin}"
-
-  remove_submodule "${bundle}" "${repo}"
-done
-
-if grep -Fq "sprsquish" "${GIT_CONFIG}" &> /dev/null; then
   remove_dir "${VIM_DIR}/bundle/thrift" \
              "plugin bundle" \
              "thrift.vim"
@@ -113,17 +105,52 @@ if grep -Fq "sprsquish" "${GIT_CONFIG}" &> /dev/null; then
              "thrift.vim"
 
   perl -i -pn \
-       -e "s/sprsquish/solarnz/" \
+       -e 's/sprsquish/solarnz/' \
        "${GIT_CONFIG}"
 
   (cd "${VIM_DIR}" && git submodule update --init)
-fi
+}
 
-find "${VIM_DIR}/bundle" \
-     -mindepth 3 \
-     -maxdepth 3 \
-     -type f \
-     -name "tags" \
-     -delete
+regenerate_helptags() {
+  find "${VIM_DIR}/bundle" \
+       -mindepth 3 \
+       -maxdepth 3 \
+       -type f \
+       -name 'tags' \
+       -delete
 
-HOME="${EFFECTIVE_HOME}" vim -c "call pathogen#helptags()" -c "qall!"
+  HOME="${EFFECTIVE_HOME}" vim -c 'call pathogen#helptags()' -c 'qall!'
+}
+
+main() {
+  build_vimproc
+
+  local plugin bundle repo
+
+  for plugin in \
+    "CamelCaseMotion:CamelCaseMotion" \
+    "clojure:VimClojure" \
+    "cocoa:cocoa.vim" \
+    "command-t:Command-T" \
+    "elm:elm-vim" \
+    "funcoo:funcoo.vim" \
+    "haml:vim-haml" \
+    "ios:vim-ios" \
+    "kiwi:kiwi.vim" \
+    "mako:mako.vim" \
+    "nginx:nginx.vim" \
+    "powerline:powerline" \
+    "systemverilog:systemverilog.vim" \
+    "ts:tsuquyomi"
+  do
+    IFS=$':' read -r bundle repo <<< "${plugin}"
+
+    remove_submodule "${bundle}" "${repo}"
+  done
+
+  migrate_thrift
+
+  regenerate_helptags
+}
+
+main "$@"
