00d0ef16db0d8eb2385bf77e495fff0ecdb71509
Dan Poggi <dan.poggi@gmail.com>
2018-07-05 18:16:55 -0400

    Plugin updates.

--- scripts/update_submodules.sh
+++ scripts/update_submodules.sh
@@ -6,70 +6,63 @@
 
 set -eo pipefail
 
-__logfln() {
-  local lvl="$1"; shift
-  local lvl_clr="$1"; shift
-  local fmt="$1"; shift
+readonly VIM_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 
-  printf "\033[2;39;49m%s ${lvl_clr}${lvl}\033[2;39;49m : \033[0m${fmt}\n" \
-         "$(date "+%Y-%m-%d %H:%M:%S")" \
-         "$@"
+__logfln() {
+  local level="$1"; shift
+  local level_color="$1"; shift
+  local format="$1"; shift
+  printf \
+    "\033[2;39;49m%s ${level_color}${level}\033[2;39;49m : \033[0m${format}\\n" \
+    "$(date "+%Y-%m-%d %H:%M:%S")" \
+    "$@"
 }
 
 infofln() { __logfln " INFO" "\033[0;34m" "$@"; }
 errorfln() { __logfln "ERROR" "\033[0;31m" "$@"; }
 
+find_submodules() {
+  local path="$1"; shift
+  find "${path}" -mindepth 1 -maxdepth 1 -type d "$@"
+}
+
+git_symbolic_ref() { git -C "$1" symbolic-ref --quiet "refs/remotes/origin/HEAD"; }
+
 update_submodule() {
-  local submodule="$1"
+  local dir name symbolic_ref
 
-  local plugin="$(basename "${submodule}")"
+  dir="$1"
+  name="$(basename "${dir}")"
 
-  pushd "${submodule}" >/dev/null
-
-  local symbolic_ref
-
-  # The JavaScript plugin's master branch is a little... behind
-  if [[ "${plugin}" = "javascript" ]]; then
-    symbolic_ref="refs/remotes/origin/develop"
-  else
-    symbolic_ref="$(git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null)"
-  fi
-
+  symbolic_ref="$(git_symbolic_ref "${dir}")"
   if [[ -z "${symbolic_ref}" ]]; then
-    errorfln "Unable to establish remote branch for %s, skipping..." "${plugin}"
-
-    popd >/dev/null
-
-    continue
+    return 1
   fi
 
-  infofln "Attempting to checkout %s for %s..." "${symbolic_ref}" "${plugin}"
+  infofln "Attempting to checkout %s for %s..." "${symbolic_ref}" "${name}"
+  git -C "${dir}" checkout --quiet --force "${symbolic_ref}"
 
-  git checkout --force "${symbolic_ref}"
+  infofln "Updating submodules for %s, if any..." "${name}"
+  git -C "${dir}" submodule --quiet update --init --recursive
 
-  infofln "Updating submodules for %s, if any..." "${plugin}"
-
-  git submodule update --init --recursive
-
-  printf "\n"
-
-  popd >/dev/null
+  printf "\\n"
 }
 
 main() {
-  cd "$(dirname "${BASH_SOURCE[0]}")/.."
+  local submodule_dir
 
   infofln "Fetching origin recursively..."
+  git -C "${VIM_DIR}" fetch --quiet --recurse-submodules origin 2>/dev/null
 
-  git fetch origin --recurse-submodules
+  printf "\\n"
 
-  printf "\n"
-
-  local submodule
-
-  while IFS='' read -d '' -r submodule; do
-    update_submodule "${submodule}"
-  done < <(find ./bundle -mindepth 1 -maxdepth 1 -type d -print0)
+  while IFS='' read -d '' -r submodule_dir; do
+    if ! update_submodule "${submodule_dir}" 2>/dev/null; then
+      errorfln "Unable to establish remote branch for %s, skipping..." "${submodule_name}"
+    fi
+  done < <(
+    find_submodules "${VIM_DIR}/bundle" -print0 2>/dev/null
+  )
 }
 
 main "$@"
